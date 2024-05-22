e94b8e7f652fea88a68d26391209eda171fd5202
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-05-22 11:25:02 +0300

    three commit

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_application/riverpod/riverpod_managament.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Home extends ConsumerWidget {
@@ -19,21 +21,49 @@ class Home extends ConsumerWidget {
               "Products",
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 20),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: read.products.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(4),
-                        child: Image.network(watch.products[index].imagePath),
-                      )
-                    ],
-                  ),
-                );
-              },
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: read.products.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Image.asset(
+                              watch.imagePaths[index],
+                              height: 200,
+                              width: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Text(
+                                  watch.products[index].title,
+                                  style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  watch.products[index].description,
+                                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
